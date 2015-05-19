b06abd36e385850e31d7d35be2bdc04768156717
claduio <c.romano007@studenti.unibs.it>
2015-05-19 15:23:18 +0200

    Gara.java

---
+++ Gara.java
@@ -0,0 +1,34 @@
+public class Gara {
+	private final static int NUMMEDAGLIE = 3;
+	private String nome;
+	private Nazione[] nazioniPremiate = new Nazione[NUMMEDAGLIE];
+	private boolean eseguita = false;
+	
+	public Gara (String nome){
+		
+		this.nome= nome;
+	}
+	
+	public void setEseguita(){
+		
+		eseguita = true;
+		
+	}
+	
+	public boolean giaPremiata (){
+		
+		return eseguita;
+	}
+	
+	public void aggiungiPremiata (Nazione nazione, int i){
+		
+		nazioniPremiate[i] = nazione;
+	}
+	
+	public Nazione[] getPremiate (){
+		
+		return nazioniPremiate;
+	}
+	
+	
+}
