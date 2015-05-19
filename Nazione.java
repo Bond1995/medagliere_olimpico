5c9837ca7a88adb82c1c1f1f01dd7443ee34ad53
Bond1995 <m.bondaschi@studenti.unibs.it>
2015-05-19 16:33:22 +0200

    Update Nazione.java

--- Nazione.java
+++ Nazione.java
@@ -1,7 +1,11 @@
+package ing.unibs.esercitazioni.medagliere;
 
 public class Nazione {
+	
+	private static final int NUMMEDAGLIE = 3;
 	private String nomeNazione;
-	private int medaglie[]= new int [3];
+	private int medaglie[]= new int [NUMMEDAGLIE];
+	private static final String MEDAGLIE_NAZIONE = "%s\t: N. ori: %d\tN. argenti: %d\tN. bronzi: %d";
 	
 	public Nazione (String nomeNazione) {
 		this.nomeNazione = nomeNazione;
@@ -20,15 +24,29 @@ public class Nazione {
 	}
 	
 	public boolean meglioDi(Nazione altraNazione){
-			int i = 0;
+		
+		int i = 0;
+		
 		do{
-			if (this.medaglie[i]> altraNazione.getMedaglie()[i]){
-				i++;
-			}
-			else 
+			
+			if (this.medaglie[i] > altraNazione.getMedaglie()[i])
+				return true;
+			else if (this.medaglie[i] < altraNazione.getMedaglie()[i]) 
 				return false;
-		}while (i<=2);
+			else i++;
+			
+		} while (i < 3);
+		
 		return true;
 	
+	}
+	
+	@Override
+	public String toString(){
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append(String.format(MEDAGLIE_NAZIONE, nomeNazione, medaglie[0], medaglie[1], medaglie[2]));
+		return builder.toString();
+		
 	}
 }
